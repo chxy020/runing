20a08568cae988807dbc2646dfa0b47a196c9fde
alwaysh <997224789@qq.com>
2014-10-12 11:05:40 +0800

    完成交接棒页面

--- src/net/yaopao/activity/MatchGroupListActivity.java
+++ src/net/yaopao/activity/MatchGroupListActivity.java
@@ -1,7 +1,5 @@
 package net.yaopao.activity;
 
-import net.yaopao.bean.DataBean;
-import android.app.Activity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
